605aa95485e8fa25f6c007effb18e461c4dd9533
napnap75 <thomas@nappez.com>
2018-01-29 11:48:46 +0100

    Update start.sh

--- snapcast-shairport/start.sh
+++ snapcast-shairport/start.sh
@@ -4,5 +4,5 @@ mkdir -p /var/run/dbus
 dbus-uuidgen --ensure
 dbus-daemon --system
 avahi-daemon --daemonize --no-chroot
-/usr/local/bin/snapserver -s "airplay:///shairport-sync?name=Airplay&devicename=$AIRPLAY_NAME"
+/usr/local/bin/snapserver -b $BUFFER_SIZE -s "airplay:///shairport-sync?name=Airplay&devicename=$AIRPLAY_NAME"
 
