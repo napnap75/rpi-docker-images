abf64c7ab423c46c654a4bbfe13ae0dfd2db61f4
Thomas Nappez <thomas@nappez.com>
2018-05-19 10:17:22 +0200

    Updated my Slack messages eraser

--- slack-eraser/slack-eraser.py
+++ slack-eraser/slack-eraser.py
@@ -2,6 +2,7 @@ import argparse
 import configparser
 import time
 import re
+import ast
 
 from slacker import Slacker
 
@@ -11,17 +12,17 @@ parser.add_argument('file', help='The configuration file')
 parser.add_argument('--verbose', '-v', action='count', help='Be verbose')
 parser.add_argument('--dry-run', '-n', action='count', help='Do nothing (for test purpose)')
 args = parser.parse_args()
-if args.verbose > 0 :
+if args.verbose != None :
 	print("Using args:", args)
 
 # Parse the configuration file
-if args.verbose > 0 :
+if args.verbose != None :
 	print("Parsing configuration file:", args.file)
 config = configparser.ConfigParser()
 config.read(args.file)
 
 # Open the connection to Slack
-if args.verbose > 0 :
+if args.verbose != None :
 	print("Opening Slack connection")
 slack = Slacker(config['DEFAULT']['SlackTocken'])
 
@@ -46,7 +47,7 @@ for sectionName in config.sections():
 		regexp = re.compile(searchRE)
 
 	if eraserType == 'duplicates':
-		if args.verbose > 0 :
+		if args.verbose != None :
 			print("Deleting duplicates messages with request: \"", searchRequest, "\", sort:", searchSort, ", order:", searchOrder, "and count:", searchCount)
 			if searchRE != None:
 				print("And using regular expression:", searchRE)
@@ -63,15 +64,15 @@ for sectionName in config.sections():
 					try:
 						text = message['attachments'][0]['text']
 					except KeyError as ex:
-						if args.verbose > 1:
+						if args.verbose != None and args.verbose > 1:
 							print("Message skipped because empty")
 						continue
 			if searchFrom != None and searchFrom != message['username']:
-				if args.verbose > 1:
+				if args.verbose != None and args.verbose > 1:
 					print(text, "--> Skipped (From)")
 				continue
 			if searchIn != None and searchIn != message['channel']['name']:
-				if args.verbose > 1:
+				if args.verbose != None and args.verbose > 1:
 					print(text, "--> Skipped (In)")
 				continue
 
@@ -83,32 +84,32 @@ for sectionName in config.sections():
 						key = match.group(1)
 					except IndexError as ex:
 						key = match.group(0)
-					if args.verbose > 1:
+					if args.verbose != None and args.verbose > 1:
 						print(text, "using key:", key)
 				else:
-					if args.verbose > 1:
+					if args.verbose != None and args.verbose > 1:
 						print(key, "--> Skipped (RegExp)")
 					continue
 
 			if key in seenMessages:
-				if args.verbose > 1:
+				if args.verbose != None and args.verbose > 1:
 					print(key, "--> Already seen")
 				if args.dry_run == None:
-					if args.verbose > 0:
+					if args.verbose != None:
 						print("Deleting message timestamp", message['ts'], "in channel", message['channel']['name'])
 					slack.chat.delete(message['channel']['id'], message['ts'])
 					time.sleep(60/50)
 				else:
-					if args.verbose > 0:
+					if args.verbose != None:
 						print("Should have deleted message timestamp", message['ts'], "in channel", message['channel']['name'])
 			else:
-				if args.verbose > 1:
+				if args.verbose != None and args.verbose > 1:
 					print(key, "--> Not seen")
 				seenMessages.append(key)
 	elif eraserType == 'alerts':
-		closedStatus = section.get('ClosedStatus', "OK")
-		openStatus = section.get('OpenStatus', "CRITICAL")
-		if args.verbose > 0 :
+		closedStatus = ast.literal_eval(section.get('ClosedStatus', "['OK', 'Success']"))
+		openStatus = ast.literal_eval(section.get('OpenStatus', "['CRITICAL', 'Failure']"))
+		if args.verbose != None :
 			print("Deleting alerts messages with request: \"", searchRequest, "\", sort:", searchSort, ", order:", searchOrder, "and count:", searchCount)
 			print("And using regular expression:", searchRE, "with opening status:", openStatus, "and closing status:", closedStatus)
 
@@ -124,61 +125,61 @@ for sectionName in config.sections():
 					try:
 						text = message['attachments'][0]['text']
 					except KeyError as ex:
-						if args.verbose > 1:
+						if args.verbose != None and args.verbose > 1:
 							print("Message skipped because empty")
 						continue
 			if searchFrom != None and searchFrom != message['username']:
-				if args.verbose > 1:
+				if args.verbose != None and args.verbose > 1:
 					print(text, "--> Skipped (From)")
 				continue
 			if searchIn != None and searchIn != message['channel']['name']:
-				if args.verbose > 1:
+				if args.verbose != None and args.verbose > 1:
 					print(text, "--> Skipped (In)")
 				continue
 
 			status = None
 			match = regexp.match(text)
 			if match:
-				if match.group(1) == closedStatus:
+				if match.group(1) in closedStatus:
 					closedAlerts.append(match.group(2))
-					if args.verbose > 1:
+					if args.verbose != None and args.verbose > 1:
 						print(text, "--> Alert '", match.group(2), "' closed")
-				elif match.group(1) == openStatus:
+				elif match.group(1) in openStatus:
 					if match.group(2) in closedAlerts:
-						if args.verbose > 1:
+						if args.verbose != None and args.verbose > 1:
 							print(text, "--> Alert '", match.group(2), "' already closed")
 					else:
-						if args.verbose > 1:
+						if args.verbose != None and args.verbose > 1:
 							print(text, "--> Alert '", match.group(2), "' not closed")
 						continue
-				elif match.group(2) == closedStatus:
+				elif match.group(2) in closedStatus:
 					closedAlerts.append(match.group(1))
-					if args.verbose > 1:
+					if args.verbose != None and args.verbose > 1:
 						print(text, "--> Alert '", match.group(1), "' closed")
-				elif match.group(2) == openStatus:
+				elif match.group(2) in openStatus:
 					if match.group(1) in closedAlerts:
-						if args.verbose > 1:
+						if args.verbose != None and args.verbose > 1:
 							print(text, "--> Alert '", match.group(1), "' already closed")
 					else:
-						if args.verbose > 1:
+						if args.verbose != None and args.verbose > 1:
 							print(text, "--> Alert '", match.group(1), "' not closed")
 						continue
 				else:
-					if args.verbose > 1:
+					if args.verbose != None and args.verbose > 1:
 						print(text, "--> Skipped (Status not found)")
 					continue
 			else:
-				if args.verbose > 1:
+				if args.verbose != None and args.verbose > 1:
 					print(text, "--> Skipped (Does not match regular expression)")
 				continue
 
 			if args.dry_run == None:
-				if args.verbose > 0:
+				if args.verbose != None:
 					print("Deleting message timestamp", message['ts'], "in channel", message['channel']['name'])
 				slack.chat.delete(message['channel']['id'], message['ts'])
 				time.sleep(60/50)
 			else:
-				if args.verbose > 0:
+				if args.verbose != None:
 					print("Should have deleted message timestamp", message['ts'], "in channel", message['channel']['name'])
 	else:
 		print("Unknown type ", eraserType, " in section", sectionName)
