89de270f399c63b234ae2e7dc18ae85d5cf8ed84
root <thomas@nappez.com>
2019-07-25 15:50:56 +0000

    First commit

--- snips-google-tts/snips-google-tts.go
+++ snips-google-tts/snips-google-tts.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"crypto/md5"
 	"crypto/tls"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -19,35 +21,10 @@ import (
 	"google.golang.org/api/option"
 )
 
-func onMessageReceived(client mqtt.Client, message mqtt.Message) {
-	fmt.Printf("Received message on topic: %s\nMessage: %s\n", message.Topic(), message.Payload())
-}
+var googleApiKey string
+var googleVoice string
 
-func mqttConnectAndSubscribe(server string, clientid string, username string, password string) {
-	connOpts := mqtt.NewClientOptions().AddBroker(server).SetClientID(clientid).SetCleanSession(true)
-	if username != "" {
-		connOpts.SetUsername(username)
-		if password != "" {
-			connOpts.SetPassword(password)
-		}
-	}
-	tlsConfig := &tls.Config{InsecureSkipVerify: true, ClientAuth: tls.NoClientCert}
-	connOpts.SetTLSConfig(tlsConfig)
-	connOpts.OnConnect = func(c mqtt.Client) {
-		if token := c.Subscribe("mytopic", 0, onMessageReceived); token.Wait() && token.Error() != nil {
-			panic(token.Error())
-		}
-	}
-	client := mqtt.NewClient(connOpts)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	} else {
-		fmt.Printf("Connected to %s\n", server)
-	}
-
-}
-
-func googleGetAudioFile(apiKey string, textInput string, voiceName string, filename string) {
+func getAudioFileFromGoogle(apiKey string, textInput string, voiceName string, filename string) {
 	// Instantiates a client.
 	ctx := context.Background()
 	client, err := texttospeech.NewClient(ctx, option.WithAPIKey(apiKey))
@@ -93,6 +70,48 @@ func googleGetAudioFile(apiKey string, textInput string, voiceName string, filen
 	log.Print("Audio content written to file: %v\n", filename)
 }
 
+func onMessageReceived(client mqtt.Client, message mqtt.Message) {
+	log.Print("Received message on topic: %s\nMessage: %s\n", message.Topic(), message.Payload())
+	
+	type Payload struct {
+		text []byte `json:"text"`
+	}
+	var messagePayload Payload
+	err := json.Unmarshal(message.Payload(), &messagePayload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if messagePayload.text != "" {
+		hash := md5.Sum(messagePayload.text)
+
+	}
+}
+
+func mqttConnectAndSubscribe(server string, clientid string, username string, password string) {
+	connOpts := mqtt.NewClientOptions().AddBroker(server).SetClientID(clientid).SetCleanSession(true)
+	if username != "" {
+		connOpts.SetUsername(username)
+		if password != "" {
+			connOpts.SetPassword(password)
+		}
+	}
+	tlsConfig := &tls.Config{InsecureSkipVerify: true, ClientAuth: tls.NoClientCert}
+	connOpts.SetTLSConfig(tlsConfig)
+	connOpts.OnConnect = func(c mqtt.Client) {
+		if token := c.Subscribe("hermes/tts/say", 0, onMessageReceived); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
+	}
+	client := mqtt.NewClient(connOpts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	} else {
+		fmt.Printf("Connected to %s\n", server)
+	}
+
+}
+
 func main() {
 	// Logs
 	mqtt.DEBUG = log.New(os.Stdout, "", 0)
@@ -102,16 +121,19 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-	server := flag.String("server", "tcp://127.0.0.1:1883", "The full url of the MQTT server to connect to")
-	clientid := flag.String("clientid", "snips-google-tts", "A clientid for the connection")
-	username := flag.String("username", "", "A username to authenticate to the MQTT server")
-	password := flag.String("password", "", "Password to match username")
-	apiKey := flag.String("apiKey", "", "Google Cloud API Key")
+	// Load the parameters
+	mqttServer := flag.String("mqtt-server", "tcp://127.0.0.1:1883", "The full url of the MQTT server to connect to")
+	mqttClientid := flag.String("mqtt-clientid", "snips-google-tts", "A clientid for the connection")
+	mqttUsername := flag.String("mqtt-username", "", "A username to authenticate to the MQTT server")
+	mqttPassword := flag.String("mqtt-password", "", "Password to match username")
+	flag.StringVar(&googleApiKey, "google-api-key", "", "Google Cloud API Key")
+	flag.StringVar(&googleVoice, "google-voice", "fr-FR-Wavenet-C", "Google TTS voice identifier")
 	flag.Parse()
 
-	mqttConnectAndSubscribe(*server, *clientid, *username, *password)
+	// Connect to MQTT
+	mqttConnectAndSubscribe(*mqttServer, *mqttClientid, *mqttUsername, *mqttPassword)
 
-	googleGetAudioFile(*apiKey, "J'ai ajoute du pain", "fr-FR-Wavenet-C", "test.mp3")
+	getAudioFileFromGoogle(googleApiKey, "J'ai ajoute du pain", googleVoice, "test.mp3")
 
 	<-c
 }
