bedf53f5caa463ca5e4fd41de3dcbfee692957bf
napnap75 <thomas@nappez.com>
2020-03-14 20:02:36 +0100

    Corrected a mistake in the PATH

--- snapcast-shairport/start.sh
+++ snapcast-shairport/start.sh
@@ -5,7 +5,7 @@ dbus-uuidgen --ensure
 dbus-daemon --system
 avahi-daemon --daemonize --no-chroot
 if [ -p /fifo/snapfifo ] ; then
-	/usr/local/bin/snapserver -b $BUFFER_SIZE -s "airplay:///shairport-sync?name=Airplay&devicename=$AIRPLAY_NAME" -s "pipe:///fifo/snapfifo?name=$STREAM_NAME"
+	/usr/bin/snapserver -b $BUFFER_SIZE -s "airplay:///shairport-sync?name=Airplay&devicename=$AIRPLAY_NAME" -s "pipe:///fifo/snapfifo?name=$STREAM_NAME"
 else
-	/usr/local/bin/snapserver -b $BUFFER_SIZE -s "airplay:///shairport-sync?name=Airplay&devicename=$AIRPLAY_NAME"
+	/usr/bin/snapserver -b $BUFFER_SIZE -s "airplay:///shairport-sync?name=Airplay&devicename=$AIRPLAY_NAME"
 fi
