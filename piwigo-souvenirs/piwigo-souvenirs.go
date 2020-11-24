42d9d775f5f6e396bedbd7db8c3e84341dc6feaa
Thomas <thomas@nappez.com>
2020-11-24 13:07:40 +0100

    Better handling of errors

--- piwigo-souvenirs/piwigo-souvenirs.go
+++ piwigo-souvenirs/piwigo-souvenirs.go
@@ -44,7 +44,7 @@ func login(wac *whatsapp.Conn, sessionFile string) error {
 		// Restore session
 		session, err = wac.RestoreWithSession(session)
 		if err != nil {
-			return fmt.Errorf("restoring failed: %v\n", err)
+			return fmt.Errorf("restoring failed: %v", err)
 		}
 	} else {
 		// No saved session -> regular login
@@ -55,14 +55,14 @@ func login(wac *whatsapp.Conn, sessionFile string) error {
 		}()
 		session, err = wac.Login(qr)
 		if err != nil {
-			return fmt.Errorf("error during login: %v\n", err)
+			return fmt.Errorf("error during login: %v", err)
 		}
 	}
 
 	// Save session
 	err = writeSession(session, sessionFile)
 	if err != nil {
-		return fmt.Errorf("error saving session: %v\n", err)
+		return fmt.Errorf("error saving session: %v", err)
 	}
 	return nil
 }
@@ -121,10 +121,10 @@ func sendMessage(wac *whatsapp.Conn, group string, message string, title string,
 	}
 	msgId, err := wac.Send(msg)
 	if err != nil {
-		return err
+		return fmt.Errorf("Error sending message with title '%s': %v", title, err)
 	}
 
-	fmt.Fprintf(os.Stdout, "Message %d sent\n", msgId)
+	fmt.Fprintf(os.Stdout, "Message with title '%s' and id '%d' sent\n", title, msgId)
 	return nil
 }
 
@@ -132,46 +132,46 @@ func testConnexions(param parameters) error {
 	// Create new WhatsApp connection and connect
 	wac, err := whatsapp.NewConn(20 * time.Second)
 	if err != nil {
-		return fmt.Errorf("Error creating connection to WhatsApp: %v\n", err)
+		return fmt.Errorf("Error creating connection to WhatsApp: %v", err)
 	}
 	err = login(wac, param.whatsappSessionFile)
 	if err != nil {
-		return fmt.Errorf("Error logging in WhatsApp: %v\n", err)
+		return fmt.Errorf("Error logging in WhatsApp: %v", err)
 	}
 	<-time.After(3 * time.Second)
 	defer wac.Disconnect()
 
 	// Prints the available groups if none provided
 	if param.whatsappGroup == "" {
-		fmt.Fprintf(os.Stdout, "No group provided, showing all available groups\n")
+		fmt.Fprintf(os.Stdout, "No WhatsApp group provided, showing all available groups\n")
 		for _, chatNode := range wac.Store.Chats {
 			fmt.Fprintf(os.Stdout, "%s | %s\n", chatNode.Jid, chatNode.Name)
 		}
 
-		return fmt.Errorf("No group provided")
+		return fmt.Errorf("No WhatsApp group provided")
 	} else {
 		_, exists := wac.Store.Chats[param.whatsappGroup]
 		if (!exists) {
-			return fmt.Errorf("Unknown group %s", param.whatsappGroup)
+			return fmt.Errorf("Unknown WhatsApp group %s", param.whatsappGroup)
 		}
 	}
 
 	// Connect to MySQL and execute a test query
 	db, err := sql.Open("mysql", param.mysqlURL + "?parseTime=true")
 	if err != nil {
-		return fmt.Errorf("Error connecting to MySQL: %v\n", err)
+		return fmt.Errorf("Error connecting to MySQL: %v", err)
 	}
 	defer db.Close()
 	results, err := db.Query("SELECT Version();")
 	if err != nil {
-		return fmt.Errorf("Error executing MySQL query: %v\n", err)
+		return fmt.Errorf("Error executing MySQL query: %v", err)
 	}
 	defer results.Close()
 
 	// Check the existence of the piwigo thumbnails directory
 	_, err = os.Stat(fmt.Sprintf("%s/galleries", param.piwigoImageFolder))
 	if err != nil {
-		return fmt.Errorf("Could not find Piwigo thumbnail directory: %v\n", err)
+		return fmt.Errorf("Could not find Piwigo thumbnail directory: %v", err)
 	}
 
 	return nil
@@ -181,11 +181,15 @@ func runLoop(param parameters) error {
 	// Create new WhatsApp connection and connect
 	wac, err := whatsapp.NewConn(20 * time.Second)
 	if err != nil {
-		return fmt.Errorf("Error creating connection to WhatsApp: %v\n", err)
+		return fmt.Errorf("Error creating connection to WhatsApp: %v", err)
 	}
 	err = login(wac, param.whatsappSessionFile)
 	if err != nil {
-		return fmt.Errorf("Error logging in WhatsApp: %v\n", err)
+		time.Sleep(30 * time.Second)
+		err = login(wac, param.whatsappSessionFile)
+		if err != nil {
+			return fmt.Errorf("Error logging in WhatsApp: %v", err)
+		}
 	}
 	<-time.After(3 * time.Second)
 	defer wac.Disconnect()
@@ -193,12 +197,12 @@ func runLoop(param parameters) error {
 	// Connect to MySQL and execute the query
 	db, err := sql.Open("mysql", param.mysqlURL + "?parseTime=true")
 	if err != nil {
-		return fmt.Errorf("Error connecting to MySQL: %v\n", err)
+		return fmt.Errorf("Error connecting to MySQL: %v", err)
 	}
 	defer db.Close()
 	results, err := db.Query("SELECT piwigo_sharealbum.code, piwigo_categories.name, representatives.path, representatives.representative_ext, MIN(piwigo_images.date_creation) AS date_creation FROM piwigo_sharealbum JOIN piwigo_categories ON piwigo_sharealbum.cat = piwigo_categories.id JOIN piwigo_image_category ON piwigo_image_category.category_id = piwigo_categories.id JOIN piwigo_images ON piwigo_image_category.image_id = piwigo_images.id JOIN piwigo_images AS representatives ON piwigo_categories.representative_picture_id = representatives.id WHERE piwigo_images.date_creation IS NOT NULL GROUP BY piwigo_categories.id;")
 	if err != nil {
-		return fmt.Errorf("Error executing MySQL query: %v\n", err)
+		return fmt.Errorf("Error executing MySQL query: %v", err)
 	}
 	defer results.Close()
 
@@ -236,7 +240,7 @@ func runLoop(param parameters) error {
 			// Send the message
 			sendMessage(wac, param.whatsappGroup, fmt.Sprintf("Il y a %d an(s) : %s", time.Now().Year()-albumDate.Year(), url), albumName, thumbnail)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error send message to WhatsApp for album '%s': %v\n", albumName, err)
+				fmt.Fprintf(os.Stderr, "Error sending message to WhatsApp for album '%s': %v\n", albumName, err)
 				continue
 			}
 		}
@@ -263,10 +267,10 @@ func main() {
 	// Test the connexion on startup
 	err := testConnexions(param)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error connecting: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Unable to connect: %v\n", err)
 		return
 	}
-	
+
 	// Run the loop everyday at 7
 	for {
 		t := time.Now()
@@ -276,12 +280,12 @@ func main() {
 			n = n.Add(24 * time.Hour)
 			d = n.Sub(t)
 		}
-		fmt.Fprintf(os.Stderr, "Sleeping for: %d\n", d)
+		fmt.Fprintf(os.Stderr, "Sleeping for: %s\n", d)
 		time.Sleep(d)
 
 		err := runLoop(param)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error connecting: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 			continue
 		}
 	}
